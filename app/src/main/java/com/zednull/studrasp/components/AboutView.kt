d432e0e67e590018b6161866eca90b598f57b10b
iamgirya <iamgirya@yandex.ru>
2021-11-03 11:20:41 +0300

    Create AboutView.kt

---
+++ app/src/main/java/com/zednull/studrasp/components/AboutView.kt
@@ -0,0 +1,142 @@
+package com.zednull.studrasp.components
+
+import android.content.Context
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.ScrollState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.ExperimentalMaterialApi
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextButton
+import androidx.compose.runtime.*
+import androidx.compose.runtime.snapshots.SnapshotStateList
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.zednull.studrasp.R
+import com.zednull.studrasp.structure.TimeTableStructure
+import com.zednull.studrasp.structure.emptyTimeTable
+import com.zednull.studrasp.structure.user
+import com.zednull.studrasp.ui.theme.TimeTableTheme
+
+@Composable
+fun AboutView(navController: NavController) {
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.appicon),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(0.dp, 32.dp, 0.dp, 0.dp)
+                .height(128.dp)
+                .width(128.dp)
+                .verticalScroll(ScrollState(0), true, null, false)
+        )
+
+        Text(
+            text = "StudRasp",
+            fontSize = 24.sp,
+            fontFamily = MaterialTheme.typography.body1.fontFamily,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.primary,
+
+            )
+
+        Text(
+            text = "v1.0 (beta)",
+            fontSize = 16.sp,
+            fontFamily = MaterialTheme.typography.body1.fontFamily,
+            fontWeight = FontWeight.Bold,
+            color = MaterialTheme.colors.onSecondary,
+            modifier = Modifier.padding(0.dp,0.dp,0.dp,32.dp)
+        )
+
+        TextButton(onClick = {
+            navController.navigate("account", )
+        },
+            modifier = Modifier
+                .padding(16.dp, 0.dp, 16.dp, 32.dp)
+                .background(MaterialTheme.colors.onPrimary, MaterialTheme.shapes.medium)
+                .fillMaxWidth()
+                .height(42.dp)
+        ) {
+            Text(
+                text = "Аккаунт",
+                fontSize = 14.sp,
+                fontFamily = MaterialTheme.typography.body1.fontFamily,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        TextButton(onClick = {
+            navController.navigate("myTimeTable")
+        },
+            modifier = Modifier
+                .padding(16.dp, 0.dp, 16.dp, 32.dp)
+                .background(MaterialTheme.colors.onPrimary, MaterialTheme.shapes.medium)
+                .fillMaxWidth()
+                .height(42.dp)
+        ) {
+            Text(
+                text = "Мои расписания",
+                fontSize = 14.sp,
+                fontFamily = MaterialTheme.typography.body1.fontFamily,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+
+        TextButton(onClick = {
+
+        },
+            modifier = Modifier
+                .padding(16.dp, 0.dp, 16.dp, 32.dp)
+                .background(MaterialTheme.colors.onPrimary, MaterialTheme.shapes.medium)
+                .fillMaxWidth()
+                .height(42.dp)
+
+        ) {
+            Text(
+                text = "О приложении",
+                fontSize = 14.sp,
+                fontFamily = MaterialTheme.typography.body1.fontFamily,
+                fontWeight = FontWeight.Medium,
+                color = MaterialTheme.colors.primary,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AboutViewPreview() {
+    TimeTableTheme{
+        val navController = rememberNavController()
+        AboutView(navController)
+    }
+}
